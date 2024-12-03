6e3cff7924315f133c8d040761c4ecfbb841a748
Gayuh <gayuhhsuh@gmail.com>
2024-12-03 14:57:36 +0700

    Delete Unused file

--- admin.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <h2>Selamat datang di dashboard Admin</h2>
-</body>
--- dosen.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <h2>Selamat datang di dashboard Dosen</h2>
-</body>
